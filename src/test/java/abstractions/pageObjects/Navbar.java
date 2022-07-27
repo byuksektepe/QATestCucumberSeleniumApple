487a65a615377156f7c05f7633991eb56aedc728
Berkantyuks <byuksektepe@hotmail.com>
2022-07-28 00:58:59 +0300

    input methods added

--- src/test/java/abstractions/pageObjects/Navbar.java
+++ src/test/java/abstractions/pageObjects/Navbar.java
@@ -1,4 +1,9 @@
 package abstractions.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class Navbar {
 }
---
+++ src/test/java/abstractions/utils/InputMethods.java
@@ -0,0 +1,60 @@
+package abstractions.utils;
+
+import abstractions.helpers.AbstractHelper;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+
+
+public class InputMethods extends AbstractHelper{
+
+    private final SelectElementByType selectElementByType = new SelectElementByType();
+
+    /**
+     * Method to enter text into text field
+     * @note not nullable
+     * @param locatorType : String : Locator type (id, name, class, xpath, css)
+     * @param text       : String : Text value to enter in field
+     * @param locatorValue : String : Locator value
+     */
+    public void enterText(String locatorType, String text, String locatorValue) {
+        getDriverWait().waitShort().until(ExpectedConditions.presenceOfElementLocated(selectElementByType.getelementbytype(locatorType, locatorValue)));
+        getDriver().findElement(selectElementByType.getelementbytype(locatorType, locatorValue)).sendKeys(text);
+    }
+
+    /**
+     * Method to clear text of text field
+     * @note not nullable
+     * @param locatorType : String : Locator type (id, name, class, xpath, css)
+     * @param locatorValue : String : Locator value
+     */
+    public void clearText(String locatorType, String locatorValue) {
+        getDriverWait().waitShort().until(ExpectedConditions.presenceOfElementLocated(selectElementByType.getelementbytype(locatorType, locatorValue)));
+        getDriver().findElement(selectElementByType.getelementbytype(locatorType, locatorValue)).clear();
+    }
+
+    /**
+     * Method to toggle check-box status
+     * @note not nullable
+     * @param locatorType : String : Locator type (id, name, class, xpath, css)
+     * @param locatorValue : String : Locator value
+     */
+    public void toggleCheckbox(String locatorType, String locatorValue) {
+        getDriverWait().waitShort().until(ExpectedConditions.presenceOfElementLocated(selectElementByType.getelementbytype(locatorType, locatorValue))).click();
+    }
+
+    /**
+     * Method to select radio button
+     * @note not nullable
+     * @param locatorType : String : Locator type (id, name, class, xpath, css)
+     * @param locatorValue : String : Locator value
+     */
+    public void selectRadioButton(String locatorType, String locatorValue) {
+        WebElement radioButton = getDriverWait().waitShort()
+                .until(ExpectedConditions.presenceOfElementLocated(selectElementByType.getelementbytype(locatorType, locatorValue)));
+        if (!radioButton.isSelected())
+            radioButton.click();
+    }
+
+
+}
