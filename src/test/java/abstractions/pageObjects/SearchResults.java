26313b72b524c56c138805db63b26f342f8a68fc
Berkantyuks <byuksektepe@hotmail.com>
2022-07-28 23:58:16 +0300

    FirstResultLocator added

--- src/test/java/abstractions/pageObjects/SearchResults.java
+++ src/test/java/abstractions/pageObjects/SearchResults.java
@@ -8,6 +8,8 @@ public class SearchResults implements IUtils{
 
     private static final String SearchResultsVerifyLocator = "//div[@class='rf-serp-explore-details']" +
                                                              "/div[@class='rf-serp-resultcount']";
+    private static final String FirstResultLocator = "//div[@class='rf-serp-explore-curated-position-1 rf-serp-curated-product']" +
+                                                     "//h2[@class='rf-serp-productname']/a";
 
     public void VerifyPageLoad(){
         driverWaits.waitUntilJSReady();
