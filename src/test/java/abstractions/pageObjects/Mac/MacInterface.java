35efe9fa1ac8ec1a757115870214ae4d5bfcc357
Berkantyuks <byuksektepe@hotmail.com>
2022-08-08 21:11:17 +0300

    MacInterface upadated for new models.

--- src/test/java/abstractions/pageObjects/Mac/MacInterface.java
+++ src/test/java/abstractions/pageObjects/Mac/MacInterface.java
@@ -1,5 +1,9 @@
 package abstractions.pageObjects.Mac;
 
+import abstractions.pageObjects.Mac.IMac.IMac;
+import abstractions.pageObjects.Mac.MacMini.MacMini;
+import abstractions.pageObjects.Mac.MacPro.MacPro;
+import abstractions.pageObjects.Mac.MacStudio.MacStudio;
 import abstractions.pageObjects.Mac.MacbookAir.MBA;
 import abstractions.pageObjects.Mac.MacbookPro.MBP;
 
@@ -7,5 +11,14 @@ public interface MacInterface {
 
     MBP mbp = new MBP();
     MBA mba = new MBA();
+
+    IMac imac = new IMac();
+
+    MacMini macmini = new MacMini();
+
+    MacStudio macstudio = new MacStudio();
+
+    MacPro macpro = new MacPro();
+
 
 }
