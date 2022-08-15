148d13f424d6b3878faed0dc150381be995ec516
Berkantyuks <byuksektepe@hotmail.com>
2022-08-16 02:23:45 +0300

    ProductHelper comments added for more readable code

--- src/test/java/abstractions/helpers/ProductHelper.java
+++ src/test/java/abstractions/helpers/ProductHelper.java
@@ -13,6 +13,12 @@ public class ProductHelper implements HelperInterface, UtilsInterface {
     private static final String AddToBagButtonLocator = "//button[@data-autom='addToCart']";
     private static final String ReviewBagButtonLocator = "//button[@title='Review Bag']";
 
+    /**
+     * Stage 1 : Method to redirect to product by given;
+     * @param Family : String : @Constants.ProductFamily -> _<Family_Name>
+     * @param ModelFamily : String : @Constants.ProductFamily -> <Family_Name>_PRODUCT_FAMILY
+     * @throws MacFamilyNotMatchByGivenException : Fail
+     */
     public void redirectToProductByGiven(String Family,
                                          String ModelFamily)
 
@@ -23,6 +29,13 @@ public class ProductHelper implements HelperInterface, UtilsInterface {
         }
     }
 
+    /**
+     * Stage 2 : Method to buy product by given;
+     * @param Family : String : @Constants.ProductFamily -> _<Family_Name>
+     * @param ModelFamily : String : @Constants.ProductFamily -> <Family_Name>_PRODUCT_FAMILY
+     * @param Model : String : @pageObjects.<Family>.<ModelFamily>.<Models> -> Model.Name
+     * @throws ModelNotMatchByGivenException : Fail
+     */
     public void buyProductByGiven(String Family,
                                   String ModelFamily,
                                   String Model)
@@ -38,6 +51,15 @@ public class ProductHelper implements HelperInterface, UtilsInterface {
         }
     }
 
+    /**
+     * Stage 3 : Method to select product configuration by given;
+     * @param Family : String : @Constants.ProductFamily -> _<Family_Name>
+     * @param ModelFamily : String : @Constants.ProductFamily -> <Family_Name>_PRODUCT_FAMILY
+     * @param Model : String : @pageObjects.<Family>.<ModelFamily>.<Models> -> Model.Name
+     * @param Config : String : @pageObjects.<Family>.<ModelFamily>.<Models> -> Model.DefinedConfigurations
+     * @throws ConfigurationNotMatchByDefinedException : Fail
+     * @throws ModelNotMatchByGivenException : Fail
+     */
     public void selectProductConfigByGiven(String Family,
                                      String ModelFamily,
                                      String Model,
@@ -57,6 +79,11 @@ public class ProductHelper implements HelperInterface, UtilsInterface {
         }
     }
 
+    /**
+     * Stage 4 : Method to get product title by given;
+     * @param Family : String : @Constants.ProductFamily -> _<Family_Name>
+     * @return : String : ProductTitle
+     */
     public String getProductTitleByGiven(String Family){
         Family = Family.toLowerCase();
         switch (Family){
@@ -65,6 +92,9 @@ public class ProductHelper implements HelperInterface, UtilsInterface {
         }
     }
 
+    /**
+     * Method to add product to bag
+     */
     public void addProductToTheBag(){
         clickMethods.click(Locators.XPath, AddToBagButtonLocator);
 
--- src/test/java/abstractions/pageObjects/Mac/Mac.java
+++ src/test/java/abstractions/pageObjects/Mac/Mac.java
@@ -11,6 +11,13 @@ import static abstractions.utils.Constants.ProductFamily.MAC_PRODUCT_FAMILY;
 public class Mac implements UtilsInterface, MacInterface {
     private static final String ChapterNavLocator = "//nav[@id='chapternav']";
 
+    /**
+     * ! Only _MAC !
+     * Method to Select Mac Family By Given MacFamily
+     * Given Mac Family must be equal pre-defined MAC_PRODUCT_FAMILY
+     * @param MacFamily : String : @Constants.ProductFamily -> MAC_PRODUCT_FAMILY
+     * @throws MacFamilyNotMatchByGivenException : Fail
+     */
     public void selectMacFamilyByGiven(String MacFamily) throws MacFamilyNotMatchByGivenException {
         MacFamily = MacFamily.toLowerCase();
         boolean isEx = true;
